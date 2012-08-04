408e4069d08b1c79e2fa35cf2502ec56e24eeded
Eiji Taguchi <etaguchi0213@gmail.com>
2012-08-04 19:21:54 +0900

    time format modified

--- bot_test.js
+++ bot_test.js
@@ -1,9 +1,18 @@
-var interval = 30 * 60 * 1000; // 10分
+var interval = 30 * 60 * 1000; // 30分
 var where = '#bot_test';
+
+
+function dateToString()
+{
+  var dt = new Date();
+  var dtText = new String();
+  dtText = dt.getHours() + ':' + dt.getMinutes() + ' です';
+  return dtText;
+}
 
 function showTime()
 {
-  send(where, new Date().toString() + ' になりました');
+  send(where, dateToString());
   setNextTimer();
 }
 
@@ -11,10 +20,11 @@ function setNextTimer()
 {
   var now = new Date().getTime();
   var next = (Math.floor(now / interval) +1) * interval;
-  setTimeout(showTime, next - now)
+  setTimeout(showTime, next - now);
 }
 
 function event::onLoad()
 {
   setNextTimer();
 }
+
